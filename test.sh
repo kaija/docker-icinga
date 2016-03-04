7417c85635e879718bcd029c33da3a0921e41aed
kaija <kaija.chang@gmail.com>
2016-03-05 00:19:28 +0800

    add extra plugin folder and remove package
    remove python-software-properties
    create nagios plugin folder

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -d -p 2080:80 -v /tmp/icinga:/etc/icinga/modules kaija/icinga
+docker run -d -p 2080:80 -v /tmp/icinga:/etc/icinga/modules -v /tmp/plugins:/usr/lib/nagios/plugins/custom --name icinga kaija/icinga
